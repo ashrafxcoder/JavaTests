aafa850c6eacb9fa1f77b93f1d2017af715021b2
Ashraf Ali <ashraf.ali1811@gmail.com>
2017-04-11 22:59:44 +0500

    Port scanning functionality abstracted by this class

--- src/com/sockets/Main.java
+++ src/com/sockets/Main.java
@@ -1,22 +1,146 @@
 package com.sockets;
 
-import java.io.IOException;
+import com.http.proto.HeaderCollection;
+
+import java.io.*;
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.StringTokenizer;
 
 /**
  * Created by Ashraf-XCODER on 4/10/2017.
  */
 public class Main {
-    public static void main(String[] args) throws IOException {
-        String host = "www.google.com";
-        String ip = getIpAddress(host);
-        System.out.println(ip);
+    public static void main(String[] args) throws Exception {
 
 
-        Socket socket = new Socket(host, 80);
+       PortScanner scanner = new PortScanner();
+       scanner.showAllOpenPorts();
 
+
+
+        
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private static Socket getSocket(String ip) throws IOException {
+        String req =
+            "GET / HTTP/1.1\n" +
+            "Host: www.bbc.com\n" +
+            "Connection: keep-alive\n" +
+            "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\n" +
+            "Accept-Encoding: gzip, deflate, sdch\n" +
+            "Accept-Language: en-US,en;q=0.8\n" ;
+        Socket socket = new Socket(ip, 80);
+        InputStream inputStream = socket.getInputStream();
+        PrintWriter writer = new PrintWriter(socket.getOutputStream());
+        writer.println(req);
+        writer.flush();
+
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+        String line = bufferedReader.readLine();
+        parseHeader(line);
+
+        while (line  != null){
+//            if (line.startsWith("Location:")){
+//                System.out.println("URL" + line.substring(line.indexOf(":")).trim());
+//            }
+            if (line.equals("")){
+                System.out.print("body started");
+            }
+            //System.out.println(line);
+            line = bufferedReader.readLine();
+        }
+        return socket;
+    }
+
+    //HTTP/1.1 301 Moved Permanently
+    //Location: http://education.oracle.com/pls/web_prod-plq-dad/db_pages.getpage?page_id=3
+
+    private static void parseHeader(String line) {
+
+        String afterHttp = line.substring(line.indexOf(" "));
+
+        int firstSpace = line.indexOf(' ');
+        if (firstSpace > 0){
+            line = line.substring(firstSpace).trim();
+            int statusCode = Integer.parseInt(line.substring(0, line.indexOf(' ')).trim());
+            System.out.println(statusCode);
+            System.out.println(line.substring(line.indexOf(' ')).trim());
+        }
+    }
+
+    public static void tokenize(Reader reader){
+        StreamTokenizer tokenizer = new StreamTokenizer(reader);
+        tokenizer.eolIsSignificant(true);
+        tokenizer.slashSlashComments(false);
+        tokenizer.ordinaryChar('/');
+        try {
+            int token = tokenizer.nextToken();
+            while (token != StreamTokenizer.TT_EOL){
+                switch (token){
+                    case StreamTokenizer.TT_NUMBER:
+                        System.out.println(tokenizer.nval);
+                        break;
+                    case StreamTokenizer.TT_WORD:
+                        System.out.println(tokenizer.sval);
+                        break;
+                    default:
+                        break;
+                }
+                token = tokenizer.nextToken();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static String getIpAddress(String host){
