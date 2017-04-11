20158ed26c849d9b0aa479765d04c0a4e738193b
Ashraf Ali <ashraf.ali1811@gmail.com>
2017-04-11 23:13:34 +0500

    Multithreaded Port scanner

---
+++ src/com/sockets/MultithreadedPortScanner.java
@@ -0,0 +1,55 @@
+package com.sockets;
+
+import java.net.InetAddress;
+import java.net.Socket;
+
+/**
+ * Created by Ashraf-XCODER on 4/11/2017.
+ */
+public class MultithreadedPortScanner extends Thread {
+
+    private int lowerPort = 0;
+    private int upperPort = 1024;
+
+    public MultithreadedPortScanner() {
+    }
+
+    public MultithreadedPortScanner(int lowerPort, int upperPort) {
+        this.lowerPort = lowerPort;
+        this.upperPort = upperPort;
+    }
+
+    public int getLowerPort() {
+        return lowerPort;
+    }
+
+    public void setLowerPort(int lowerPort) {
+        this.lowerPort = lowerPort;
+    }
+
+    public int getUpperPort() {
+        return upperPort;
+    }
+
+    public void setUpperPort(int upperPort) {
+        this.upperPort = upperPort;
+    }
+
+    public void showAllOpenPorts(){
+
+        for (int j = lowerPort; j < upperPort; j++) {
+            try {
+                Socket socket = new Socket(InetAddress.getLocalHost(), j);
+                System.out.println("*******************port in use " + j);
+            }catch (Exception e){
+                System.out.println("port not in use " + j);
+            }
+        }
+    }
+
+
+    @Override
+    public void run() {
+        showAllOpenPorts();
+    }
+}
